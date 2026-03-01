575ea06bdec69abafdf78b669e7e6f0b4ec9dbb6
Ankush Yadav <anyadav8052@gmail.com>
2026-03-01 23:41:20 +0530

    Update auth.jsx

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Auth from "./pages/auth.jsx";
+import Auth from "./pages/auth";
 import { BsRobot } from "react-icons/bs";
 import { IoSparkles } from "react-icons/io5";
 import { motion } from "motion/react"
